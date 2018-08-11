eb5cbaadcb2245648929483a88b23af7f49199f2
jpillet <jpillet@gmail.com>
2018-08-11 12:15:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = "is the new champion!";
+// document.body.appendChild(element);
+ 
+var x = document.getElementById('main');
+x.remove();
+y = document.createElement('h1');
+y.setAttribute("id","victory");
